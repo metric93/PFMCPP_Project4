86c396bf2f0c0d13a185240ca0b7623b7ef73ad4
matkatmusic <matkatmusic@gmail.com>
2020-03-13 22:33:01 +0000

    revised instructions

--- main.cpp
+++ main.cpp
@@ -4,6 +4,8 @@
  Returning Values tasks 
 
  Create a branch named Part1
+
+ Purpose:  This project will take you through the process of writing a class that wraps a numeric type, beginning with writing simple member functions and ending with a fully templated class with lots of functionality. 
  
  1) write 3 UDTs named FloatType, DoubleType, IntType.
  
@@ -13,8 +15,10 @@
         multiply( lhs, rhs );
         divide( lhs, rhs );
  3) implement the appropriate action in the member function. 
-         Be aware that floating point division by 0 is legal, but integer division by 0 will crash your program.  
-         Handle this possible input when you write your divide() functions.
+         a) Be aware that floating point division by 0 is legal, but integer division by 0 will crash your program.  
+         b) Handle this possible input when you write your divide() functions.
+         c) you should warn the user if they're doing floating-point-division-by-zero but not prevent the division from happening
+         d) you should warn AND prevent the division from happening if it is an integer-division-by-zero.
  
  4) make them return the correct primitive type. e.g. if you're implementing the FloatType::add function, your implementation would start like this:
         float FloatType::add( float lhs, float rhs )
@@ -30,7 +34,9 @@
  
  Commit your changes by clicking on the Source Control panel on the left, entering a message, and click [Commit and push].
  
- Send me the the link to your repl.it in a DM on Slack
+Make a pull request after you make your first commit and pin the pull request link to our DM thread.
+
+send me a DM to check your pull request
 
  Wait for my code review.
  */
